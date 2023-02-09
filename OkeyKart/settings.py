a7df860e29f4084a2f5fbfd8c4ee0a796c893347
diman <singhdiman92@gmail.com>
2023-02-10 00:15:51 +0530

    django Auth

--- OkeyKart/settings.py
+++ OkeyKart/settings.py
@@ -145,3 +145,17 @@ MEDIA_ROOT = BASE_DIR/'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+# SMPT configration
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'dimansingh03012003@gmail.com'
+EMAIL_HOST_PASSWORD = 'hhes tlni chbb qlui'
+EMAIL_USE_TLS = True
