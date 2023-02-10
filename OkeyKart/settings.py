9cfcee90f66a1a711464cae9cafa8f4da542a8bb
diman <singhdiman92@gmail.com>
2023-02-11 02:00:17 +0530

    handel checkout page

--- OkeyKart/settings.py
+++ OkeyKart/settings.py
@@ -157,5 +157,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dimansingh03012003@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'hhes tlni chbb qlui'
 EMAIL_USE_TLS = True
