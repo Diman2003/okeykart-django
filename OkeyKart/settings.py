8d516bc9bcb4211890a74f3ba37d347199b0a0ee
diman <dimansingh03012003@gmail.com>
2023-02-14 06:18:22 +0530

    review system

--- OkeyKart/settings.py
+++ OkeyKart/settings.py
@@ -158,5 +158,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dimansingh03012003@gmail.com'
-EMAIL_HOST_PASSWORD = 'hhes tlni chbb qlui'
+EMAIL_HOST_PASSWORD = 'Diman@2003'
 EMAIL_USE_TLS = True
